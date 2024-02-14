80f6cc0f75c37fef79f4c8ea87f8ba1ac979c289
Saskham Negi <negisaksham363@gmail.com>
2024-02-14 16:18:30 +0530

    Added deployed server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Main from "./components/Main";
 
-const socket = io.connect("http://localhost:8000");
+const socket = io.connect("https://chat-app-server-opgs.onrender.com/");
 
 function App() {
   const [username, setUsername] = useState("");
