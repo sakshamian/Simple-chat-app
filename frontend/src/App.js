1f07b8a9a6edc6bf0276ca4d71094e17632a2373
Saskham Negi <negisaksham363@gmail.com>
2024-02-14 16:10:31 +0530

    All files commit

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Main from "./components/Main";
+
+const socket = io.connect("http://localhost:8000");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [showChat, setShowChat] = useState(false);
+  const room = "1234";
+
+  const startChat = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <input
+            type="text"
+            placeholder="Enter your name..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <button onClick={startChat}>Start Chatting</button>
+        </div>
+      ) : (
+        <Main socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
+}
+
+export default App;
